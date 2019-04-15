9fb8e8bc1b503e99618580791d89d41f6b920637
Julio Bell <juliobell2014@gmail.com>
2019-04-14 23:13:37 -0400

    files updated
    Signed-off-by: Julio Bell <juliobell2014@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,18 @@ var topics = ["dog", "cat", "rabbit", "hamster", "goldfish", "bird", "turtle", "
             "eagle", "crocodile", "cow", "horse", "lion", "panther", "dolphin", "jaguar"];
 
 function createButtons() {
+    // var btnGroup = $("<div class='btn-group btn-group-lg mr-2' role='group'>");
     for (var i = 0; i < topics.length; i++) {
-        var animal = topics[i];
-        var button = $("<button class='btn btn-lg btn-info' data-animal=" + animal + ">" + animal + "</button>");
+        var animal = topics[i];            
+        var button = $("<button class='btn btn-lg btn-info mr-1 py-1' data-animal=" + animal + ">" + animal + "</button>");
+        // btnGroup.append(button);   
         $("#buttons").append(button);
     }
 }
+
+ 
 $(document).ready(function(){
+
     createButtons();
 
     $("button").on("click", function() {
@@ -24,21 +29,36 @@ $(document).ready(function(){
             var results = response.data;
             console.log(results);
             for (var i = 0; i < results.length; i++) {
-    
-                var animalDiv = $("<div>");
+                var col = $("<div class='col-12 col-sm-6 col-lg-3'>");
+                var animalDiv = $("<div class='card mb-2'>");
+                var cardBody = $("<div class='card-body'>");
+
                 var rating = results[i].rating;
                 var title = results[i].title;
                 var p1 = $("<p>").text("Rating: " + rating);
-                var p2 = $("<p>").text("Title: " + title);
-                var animalImage = $("<img>").addClass(".gif");
+
+                // var cardTitle = $("<h6 class='card-text'>");
+                //     cardTitle.text("Title: " + title);
+
+                var animalImage = $("<img>").addClass(".gif .img-fluid");
     
                 animalImage.attr("src", results[i].images.fixed_height.url);
-                animalDiv.append(p1);
-                animalDiv.append(p2);
+                animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+                animalImage.attr("data-state", "still");
+                console.log(animalImage);
+                
+                
                 animalDiv.append(animalImage);
+                animalDiv.append(cardBody);
+                // animalDiv.append(cardTitle);
+                animalDiv.append(p1);
 
-                var col = $("<div class='col-sm-3'>")
                 col.append(animalDiv);
+                
+
+                // var card = $("<div class='card'>")
+                //     card.append(animalDiv);
+                
                 $("#gifs-appear-here").prepend(col);
             }
     
@@ -47,6 +67,7 @@ $(document).ready(function(){
 
     $(".gif").on('click', function(){
 
+        // var state = $(this).attr("data-state");
         var state = $(this).attr("data-state");
     
           if (state === "still") {
